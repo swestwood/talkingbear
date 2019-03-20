68f3a61fb6e6b0e4c85aafc53cee0f878cd32e26
Lillian Michalek <lillianfmichalek@gmail.com>
2019-03-19 21:31:10 -0700

    Translation change, forgot talk.js file

--- talk.js
+++ talk.js
@@ -10,47 +10,66 @@
  */
 
 const fs = require('fs');
+const util = require('util');
 const kTalkFile = "generated_talk.mp3";
+const projectId = 'talkingbear';
 
 // Google Cloud client library
-const textToSpeech = require('@google-cloud/text-to-speech');
-
-const client = new textToSpeech.TextToSpeechClient();
 
 // The text to synthesize, passed from the command line
 var args = process.argv.slice(2);
-const text = args.length > 0 ? args[0] : 'Hello, world!';
+const language = args.length > 0 ? args[0] : "en";
+const text = args.length > 1 ? args[1] : 'Hello, world!';
 
-console.log("Converting: " + text)
+var lang = 'en-AU';
+var voice = 'en-AU-Standard-C'
 
-const request = {
-    input: {text: text},
-    voice: {
-        languageCode: 'en-AU',
-        ssmlGender: 'FEMALE',
-        name: "en-AU-Standard-C",
-    },
-    audioConfig: {
-        audioEncoding: 'MP3',
-        "pitch": "6.0",
-        speakingRate: .9
-    }
-};
+console.log("Converting phrase: "+text+", in language: "+language);
+translate(text, language);
 
-client.synthesizeSpeech(request, (err, response) => {
-    if (err) {
-        console.error('Error:', err);
+
+async function translate(text, language) {
+    if (language == "en") {
+        await convertToSpeech(text);
         return;
     }
 
+    const {Translate} = require('@google-cloud/translate');
+    const translationClient = new Translate({projectId});
+
+    console.log("Translating: " + text)
+    const [translation] = await translationClient.translate(text, language);
+    console.log("Translated text: "+ translation);
+    await convertToSpeech(translation);
+}
+
+async function convertToSpeech(text) {
+    const textToSpeech = require('@google-cloud/text-to-speech');
+    const voiceClient = new textToSpeech.TextToSpeechClient();
+
+    const request = {
+        input: {text: text},
+        voice: {
+            languageCode: lang,
+            ssmlGender: 'FEMALE',
+            name: voice,
+        },
+        audioConfig: {
+            audioEncoding: 'MP3',
+            "pitch": "6.0",
+            speakingRate: .9
+        }
+    };
+
+    console.log("Converting to voice: " + text)
+    const [response] = await voiceClient.synthesizeSpeech(request);
     // Write the binary audio content to a local file
     // File name is used in speech_echo.py as well
-    fs.writeFile(kTalkFile, response.audioContent, "binary", err => {
-        if (err) {
-            console.error('Error:', err);
-            return;
-        }
-        console.log("Audio content written to file: " + kTalkFile);
-    });
-});
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile(kTalkFile, response.audioContent, 'binary');
+    console.log("Audio content written to file: " + kTalkFile);
+}
+
+
+
 
